2dab1d4f4fa93d085af16a546569573c27781b2e
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-10-07 22:55:27 +0500

    some directory commands

---
+++ Part_01/01_First-Script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Above notation is called as shebang
+
+#create a folder first
+mkdir NewFolder
+
+#now change directory in it
+cd NewFolder
+
+#create two files init
+touch FirstFile.ts SecondFile.py
+
+echo "Folder created successfully"
